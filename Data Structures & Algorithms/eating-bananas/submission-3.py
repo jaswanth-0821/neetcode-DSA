219ea71e115af2e79969f1216290a3c8bce10745
JASWANTH REDDY .P <jassu0821p@gmail.com>
2026-04-29 05:50:28 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,22 @@
+import math
+class Solution:
+    def check(self,piles , h, target):
+        time = [math.ceil(p/target) for p in piles]
+        return sum(time)<=h
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+      
+        l = 1
+        r = max(piles)
+        res = r
+        while(l<=r):
+            m = l + (r-l) // 2
+
+         
+            if self.check(piles, h , m):
+                res = m
+                r = m-1
+            else:
+                l = m+1
+       
+        return res
